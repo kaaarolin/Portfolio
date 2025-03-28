86cebc610f33962e0f0f2b7426f68168926a1aa6
kaaarolin <karolin_57@hotmail.com>
2025-03-28 16:27:45 +0100

    new changes

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+/* Scroll animation */
+
+ScrollReveal({
+    reset: true,             
+    distance: '60px',        
+    duration: 1000,          
+    delay: 200               
+  });
+  
+
+  ScrollReveal().reveal('.section__text', { origin: 'left' });
+  ScrollReveal().reveal('.section__pic-container', { origin: 'right' });
+  ScrollReveal().reveal('.about-pic', { origin: 'bottom' });
+  ScrollReveal().reveal('.details-container', { origin: 'top' });
+  ScrollReveal().reveal('.text-container', { origin: 'top' });
+  ScrollReveal().reveal('.btn', { origin: 'bottom' });
+  ScrollReveal().reveal('.icon', { origin: 'bottom', interval: 100 });
+  
